7e8de8eadfbd0be3ab4b73084186c52380a4c0fb
Kalutu <kalutudaniel@gmail.com>
2025-01-03 23:57:03 +0300

    enhance UX

--- rentals/rentals/doctype/ride_order/ride_order.js
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2025, Kalutu and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Order", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Ride Order", {
+	refresh(frm) {
+		if (frm.doc.status == "New") {
+			frm.add_custom_button(
+				"Accept",
+				() => {
+					// change status to accepted
+					frm.set_value("status", "Accepted");
+					// Save form
+					frm.save();
+				},
+				"Actions"
+			);
+			frm.add_custom_button(
+				"Reject",
+				() => {
+					// change status to accepted
+					frm.set_value("status", "Rejected");
+					// Save form
+					frm.save();
+				},
+				"Actions"
+			);
+		}
+	},
+});
