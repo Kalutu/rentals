090a23527db5a25a0553d83b1c701e1ef114e3fa
Kalutu <kalutudaniel@gmail.com>
2025-01-06 16:35:44 +0300

    test: unit tests

--- rentals/rentals/doctype/driver/driver.py
+++ rentals/rentals/doctype/driver/driver.py
@@ -7,4 +7,7 @@ from frappe.website.website_generator import WebsiteGenerator
 
 class Driver(WebsiteGenerator):
 	def before_save(self):
-		self.full_name = f"{self.first_name} {self.last_name}"	
+		if self.last_name:
+			self.full_name = f"{self.first_name} {self.last_name}"	
+		else:
+			self.full_name = self.first_name
